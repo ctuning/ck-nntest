619a65554e4b633ad24f53836c82e1bba9be120e
Nikolay Chunosov <nikolay.chunosov@xored.com>
2018-06-18 18:26:40 +0700

    directconv

---
+++ program/directconv-armcl-opencl/cmake-build-debug/CMakeFiles/directconv_armcl_opencl.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nikolay/CK/ck-acl-private/program/directconv-armcl-opencl/autotune/tuner_direct_convolution.cpp" "/home/nikolay/CK/ck-acl-private/program/directconv-armcl-opencl/cmake-build-debug/CMakeFiles/directconv_armcl_opencl.dir/autotune/tuner_direct_convolution.cpp.o"
+  "/home/nikolay/CK/ck-acl-private/program/directconv-armcl-opencl/directconv.cpp" "/home/nikolay/CK/ck-acl-private/program/directconv-armcl-opencl/cmake-build-debug/CMakeFiles/directconv_armcl_opencl.dir/directconv.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARMCL_18_05_PLUS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/nikolay/CK-TOOLS/lib-armcl-opencl-private-dividiti-master-gcc-6.4.0-linux-64/src"
+  "/home/nikolay/CK-TOOLS/lib-armcl-opencl-private-dividiti-master-gcc-6.4.0-linux-64/src/utils"
+  "/home/nikolay/CK-TOOLS/lib-armcl-opencl-private-dividiti-master-gcc-6.4.0-linux-64/src/include"
+  "/home/nikolay/CK-TOOLS/lib-nntest-0.1-gcc-6.4.0-linux-64/install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ program/directconv-armcl-opencl/cmake-build-debug/CMakeFiles/directconv_armcl_opencl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for directconv_armcl_opencl.
+# This may be replaced when dependencies are built.
