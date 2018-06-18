7fdf23495512e62002334fc29725a70dae4a268d
Nikolay Chunosov <nikolay.chunosov@xored.com>
2018-06-18 18:31:26 +0700

    resizebilenear

---
+++ program/resizebilinear-armcl-opencl/cmake-build-debug/CMakeFiles/resizebilinear_armcl_opencl.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nikolay/CK/ck-acl-private/program/resizebilinear-armcl-opencl/autotune/tuner_scale.cpp" "/home/nikolay/CK/ck-acl-private/program/resizebilinear-armcl-opencl/cmake-build-debug/CMakeFiles/resizebilinear_armcl_opencl.dir/autotune/tuner_scale.cpp.o"
+  "/home/nikolay/CK/ck-acl-private/program/resizebilinear-armcl-opencl/resizebilinear.cpp" "/home/nikolay/CK/ck-acl-private/program/resizebilinear-armcl-opencl/cmake-build-debug/CMakeFiles/resizebilinear_armcl_opencl.dir/resizebilinear.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARMCL_18_05_PLUS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/nikolay/CK-TOOLS/lib-armcl-opencl-private-dividiti-master-gcc-6.4.0-linux-64/src"
+  "/home/nikolay/CK-TOOLS/lib-armcl-opencl-private-dividiti-master-gcc-6.4.0-linux-64/src/utils"
+  "/home/nikolay/CK-TOOLS/lib-armcl-opencl-private-dividiti-master-gcc-6.4.0-linux-64/src/include"
+  "/home/nikolay/CK-TOOLS/lib-nntest-0.1-gcc-6.4.0-linux-64/install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ program/resizebilinear-armcl-opencl/cmake-build-debug/CMakeFiles/resizebilinear_armcl_opencl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/resizebilinear_armcl_opencl.dir/autotune/tuner_scale.cpp.o"
+  "CMakeFiles/resizebilinear_armcl_opencl.dir/resizebilinear.cpp.o"
+  "resizebilinear_armcl_opencl.pdb"
+  "resizebilinear_armcl_opencl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/resizebilinear_armcl_opencl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
