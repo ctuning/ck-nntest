45f681880254fc20764b78a95280e7ea72eef186
Chunosov <N.Chunosov@yandex.ru>
2018-07-26 16:32:20 +0700

    Select different LWS tuners (#15)
    * generalized tuner selection
    
    * fix tuner selection
    
    * check if arch is bifrost

--- program/reshape-armcl-opencl/reshape.cpp
+++ program/reshape-armcl-opencl/reshape.cpp
@@ -18,10 +18,9 @@ using namespace arm_compute;
 
 int main() {
   init_test();
-  arm_compute::ICLTuner *tuner = nullptr;
-  const bool find_optimal_lws = static_cast<bool>(getenv("CK_FIND_OPTIMAL_LWS"));
-  if(find_optimal_lws) tuner = new arm_compute::CLTuner_Reshape();
-  init_armcl(tuner);
+
+  auto tuner = get_lws_tuner<CLTuner_Reshape>();
+  init_armcl(tuner.get());
 
   Shape in_shape = get_input_shape_from_env();
 
@@ -74,7 +73,6 @@ int main() {
 
   input.allocator()->free();
   output.allocator()->free();
-  if(find_optimal_lws) delete tuner;
   finish_test();
   return 0;
 }
